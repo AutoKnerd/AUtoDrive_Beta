1593b421869800740c90861a0456c9aceebcc396
Andrew Sardone <andrew@autoknerd.com>
2026-04-01 11:56:13 -0400

    Add Google Tag Manager sitewide tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 
 import type { Metadata } from 'next';
+import Script from 'next/script';
 import { AuthProvider } from '@/context/auth-provider';
 import { Toaster } from '@/components/ui/toaster';
 import './globals.css';
@@ -7,6 +8,7 @@ import { MainLayout } from '@/components/layout/main-layout';
 import { FirebaseClientProvider } from '@/firebase/client-provider';
 import { ThemeProvider } from '@/context/theme-provider';
 
+const GTM_ID = 'GTM-T23TJBN6';
 
 export const metadata: Metadata = {
   title: 'AutoDrive CX',
@@ -33,19 +35,36 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
+        <Script id="google-tag-manager" strategy="beforeInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','${GTM_ID}');
+          `}
+        </Script>
         <script dangerouslySetInnerHTML={{ __html: themeSyncScript }} />
       </head>
       <body className="antialiased">
-          <FirebaseClientProvider>
-            <AuthProvider>
-              <ThemeProvider>
-                <MainLayout>
-                    {children}
-                </MainLayout>
-                <Toaster />
-              </ThemeProvider>
-            </AuthProvider>
-          </FirebaseClientProvider>
+        <noscript>
+          <iframe
+            src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
+        <FirebaseClientProvider>
+          <AuthProvider>
+            <ThemeProvider>
+              <MainLayout>
+                {children}
+              </MainLayout>
+              <Toaster />
+            </ThemeProvider>
+          </AuthProvider>
+        </FirebaseClientProvider>
       </body>
     </html>
   );
